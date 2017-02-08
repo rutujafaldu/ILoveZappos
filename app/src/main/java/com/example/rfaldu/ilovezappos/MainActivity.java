1b79d9b528e0029d0259cb4f469aead1e5324ae6
rutujafaldu <faldurutuja@gmail.com>
2017-02-07 23:23:54 -0700

    Animations done

--- app/src/main/java/com/example/rfaldu/ilovezappos/MainActivity.java
+++ app/src/main/java/com/example/rfaldu/ilovezappos/MainActivity.java
@@ -2,6 +2,7 @@ package com.example.rfaldu.ilovezappos;
 
 
 import android.databinding.DataBindingUtil;
+import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.SearchView;
@@ -13,7 +14,11 @@ import android.view.View;
 import android.view.animation.Animation;
 import android.view.animation.AnimationUtils;
 import android.support.design.widget.FloatingActionButton;
+import android.view.animation.BounceInterpolator;
+import android.view.animation.DecelerateInterpolator;
+import android.view.animation.TranslateAnimation;
 import android.widget.TextView;
+import android.widget.Toast;
 
 
 import com.example.rfaldu.ilovezappos.databinding.ActivityMainBinding;
@@ -67,22 +72,26 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         int id = v.getId();
         switch (id){
             case R.id.fab:
-
                 animateFAB();
                 break;
             case R.id.fab1:
-
+                Toast.makeText(v.getContext(), "Added to Cart", Toast.LENGTH_SHORT).show();
+                animateFab1();
                 Log.d("Raj", "Fab 1");
                 break;
             case R.id.fab2:
                 if(fab2_status == false) {
+                    Toast.makeText(v.getContext(), "Added to Favourites", Toast.LENGTH_SHORT).show();
                     fab2.setImageResource(R.drawable.ic_favorite_white_24dp);
                     fab2.startAnimation(zoom_in);
-                    //fab2.startAnimation(zoom_out);
+                    fab2.startAnimation(zoom_out);
+                    //Snackbar.make(v, "Added to Favourites",Snackbar.LENGTH_LONG).setAction("Action",null).show();
                     fab2_status = true;
                 }
                 else{
+                    Toast.makeText(v.getContext(), "Removed from Favourites", Toast.LENGTH_SHORT).show();
                     fab2.setImageResource(R.drawable.ic_favorite_border_white_24dp);
+                    //Snackbar.make(v, "Removed from Favourites",Snackbar.LENGTH_LONG).setAction("Action",null).show();
                     fab2_status = false;
                 }
                 Log.d("Raj", "Fab 2");
@@ -107,6 +116,8 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
 
             fab.startAnimation(rotate_forward);
             fab1.startAnimation(fab_open);
+            fab1.setScaleX(0.8f);
+            fab1.setScaleY(0.8f);
             fab2.startAnimation(fab_open);
             fab1.setClickable(true);
             fab2.setClickable(true);
@@ -114,6 +125,35 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
             Log.d("Raj","open");
 
         }
+    }
+
+    public  void  animateFab1(){
+        final int deltaY = 200;
+
+        TranslateAnimation jump = new TranslateAnimation(0,0,0, -deltaY);
+        jump.setFillAfter(true);
+        jump.setDuration(400);
+        jump.setInterpolator(new DecelerateInterpolator(2.0f));
+
+        jump.setAnimationListener(new Animation.AnimationListener() {
+            public void onAnimationStart(Animation animation)  {
+                fab1.setScaleX(1.0f);
+                fab1.setScaleY(1.0f);
+            }
+            public void onAnimationRepeat(Animation animation) {}
+            public void onAnimationEnd(Animation animation) {
+                TranslateAnimation bounceBack = new TranslateAnimation(0,0,-deltaY, 0);
+                bounceBack.setFillAfter(true);
+                bounceBack.setDuration(500);
+                bounceBack.setInterpolator(new BounceInterpolator());
+                fab1.setScaleX(0.8f);
+                fab1.setScaleY(0.8f);
+                fab1.startAnimation(bounceBack);
+
+            }
+        });
+
+        fab1.startAnimation(jump);
     }
 
     @Override
@@ -136,6 +176,7 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
                 displayProduct(query);
                 brandName_TextView.setVisibility(View.VISIBLE);
                 price_TextView.setVisibility(View.VISIBLE);
+                fab.setVisibility(View.VISIBLE);
                 fab2.setImageResource(R.drawable.ic_favorite_border_white_24dp);
                 fab2_status = false;
                 return false;
@@ -145,6 +186,7 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
             public boolean onQueryTextChange(String newText) {
                 //searchItem = newText;
                 //t1.setText(searchItem);
+                //displayProduct(newText);
                 return false;
 
             }
